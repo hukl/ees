007d17bfaf0f636f18f5bdedb165c3a52ff2954e
hukl <contact@smyck.org>
2011-12-04 17:56:59 +0100

    added basic app generator. no erb interpolation yet

--- lib/ees/generators.rb
+++ lib/ees/generators.rb
@@ -11,7 +11,21 @@ module Ees
        end
 
       def generate_app options
+        unless Dir.exists?( File.join( options[:path], "apps" ) )
+          puts "Run this command from the project root"
+          return
+        end
 
+        template_path = File.join( Ees::Api.template_path, "app", ".")
+        app_path = File.join( options[:path], "apps", options[:argument] )
+
+        if Dir.exists?( app_path )
+          puts "Application already exists in #{app_path}"
+          return
+        end
+
+        FileUtils.mkdir( app_path )
+        FileUtils.cp_r( template_path, app_path )
       end
 
       def generate_behavior options
